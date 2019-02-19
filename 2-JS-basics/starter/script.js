68fb339f55b1fbc25307dfcf47dcc214bc822c52
Evan <evanmlongwood@gmail.com>
2019-02-19 14:53:34 -0800

    Truthy and Falsy Values and Equality Operators.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -237,6 +237,7 @@ if (age >= 18) {
 */
 
 // Switch statement
+/*
 var job = 'instructor';
 switch (job) {
     case 'teacher':
@@ -266,7 +267,25 @@ switch (true) {
         break;
     default:
         console.log(firstName  + ` is a man`);
+        }
+*/
 
+/******************
+* Truthy and Falsy values and equality operators.
+ */
+
+// falsy values: undefined, null, 0, ``, NaN
+// truthy values: NOT falsy values
+
+var height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined.')
+} else {
+    console.log('Variable has NOT been defined.')
 }
 
-
+// Equality operators
+if (height == '23') {
+    console.log('The == operator does type coercion!');
+}
