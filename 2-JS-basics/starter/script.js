c52511cbf09cb304eac85737a9bcc2921d809de6
Evan <evanmlongwood@gmail.com>
2019-02-19 16:28:07 -0800

    Coding challenge completed.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -277,6 +277,7 @@ switch (true) {
 // falsy values: undefined, null, 0, ``, NaN
 // truthy values: NOT falsy values
 
+/*
 var height = 23;
 
 if (height || height === 0) {
@@ -286,6 +287,43 @@ if (height || height === 0) {
 }
 
 // Equality operators
-if (height == '23') {
+if (height === '23') {
     console.log('The == operator does type coercion!');
 }
+*/
+
+/******************************
+* CODING CHALLENGE
+ */
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 116, 94, and 123 points.
+
+1. Calculate the average score for each team.
+2. Decide which teams wins in average (highest average score), and print the winner to the console.
+Also include the average score in the output.
+3. Then change the score to show different winners. Don't forget to take into account there might be a draw.
+(the same average score.)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134, and 105 points. Like before, log the
+average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one,
+just watch the solution, it's no problem.
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK! :D
+ */
+
+var johnTeamAverage = (89 + 120 + 103) / 3;
+var mikeTeamAverage = (116 + 94 + 123) / 3;
+var maryTeamAverage = (97 + 134 + 105) / 3;
+
+if(johnTeamAverage > mikeTeamAverage && maryTeamAverage){
+    console.log(`John's team wins in average score. Their total score is ${johnTeamAverage}`)
+} else if (johnTeamAverage && mikeTeamAverage < maryTeamAverage) {
+    console.log(`Mary's team wins in average score. Their total score is ${maryTeamAverage}`)
+} else if (mikeTeamAverage > johnTeamAverage && maryTeamAverage) {
+    console.log(`Mike's team wins in average score. Their total score is ${mikeTeamAverage}`);
+} else {
+    console.log(`They all have the same score.`)
+}
+
