269d82d0f270351edd046ddc14b48f751c9ba6f2
Evan <evanmlongwood@gmail.com>
2019-02-20 12:23:42 -0800

    Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -368,6 +368,7 @@ yearsUntilRetirement(1969, 'Jane');
    // function whatDoYouDo (job, firstName) {}
 
 // Function expression
+/*
 var whatDoYouDo = function(job, firstName) {
     switch (job) {
         case 'teacher':
@@ -382,3 +383,40 @@ var whatDoYouDo = function(job, firstName) {
 };
 
 console.log(whatDoYouDo('cop', 'John'));
+*/
+
+/**************************************`
+* Arrays
+ */
+
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher',
+    false];
+
+// add elements
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+//remove elements
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
