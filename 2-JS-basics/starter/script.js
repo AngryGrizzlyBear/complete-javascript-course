fe344c1e64125036c44f5109c98ee75a5fecebba
Evan <evanmlongwood@gmail.com>
2019-02-20 17:27:53 -0800

    Coding Challenge. I finished it, will add the true answer (if they have the same BMI tomorrow.)

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -501,6 +501,7 @@ console.log(jane);
 * Objects and methods
  */
 
+/*
 var john = {
   firsName: 'John',
   lastName: 'Smith',
@@ -515,3 +516,59 @@ var john = {
 
 john.calcAge();
 console.log(john);
+*/
+
+/*****************************
+ * CODING CHALLENGE 4
+ */
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+GOOD LUCK 😀
+*/
+
+var mark = {
+  firstName: 'Mark',
+  lastName: 'Johnson',
+  mass: 50,
+  height: 1.9,
+  markBMI: function() {
+   this.bmi = this.mass / (this.height *= 2);
+   return this.bmi
+  }
+};
+
+var john = {
+  firstName: 'John',
+  lastName: 'Markson',
+  mass: 60,
+  height: 1.7,
+  johnBMI: function() {
+    this.bmi = this.mass / (this.height *= 2);
+    return this.bmi
+  }
+};
+
+mark.markBMI() > john.johnBMI() ? console.log(`${mark.firstName + ' ' + mark.lastName} has a higher BMI, sitting at ${mark.markBMI()}`) :
+    console.log(`${john.firstName + ' ' + john.lastName} has a higher BMI, sitting at ${john.johnBMI()}`);
+// var john = {
+//   firsName: 'John',
+//   lastName: 'Smith',
+//   birthYear: 1992,
+//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
+//   job: 'teacher',
+//   isMarried: false,
+//   calcAge: function() {
+//     this.age = 2018 - this.birthYear;
+//   }
+// };
+//
+// john.calcAge();
+// console.log(john);
+// John = {};
