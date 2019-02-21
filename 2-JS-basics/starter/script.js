1e004278d3a1b65239bb21c58f75cdb831971783
Evan <evanmlongwood@gmail.com>
2019-02-21 10:22:58 -0800

    Loops and iterations.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -533,6 +533,7 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 GOOD LUCK 😀
 */
 
+/*
 var mark = {
   firstName: 'Mark',
   lastName: 'Johnson',
@@ -557,18 +558,52 @@ var john = {
 
 mark.markBMI() > john.johnBMI() ? console.log(`${mark.firstName + ' ' + mark.lastName} has a higher BMI, sitting at ${mark.markBMI()}`) :
     console.log(`${john.firstName + ' ' + john.lastName} has a higher BMI, sitting at ${john.johnBMI()}`);
-// var john = {
-//   firsName: 'John',
-//   lastName: 'Smith',
-//   birthYear: 1992,
-//   family: ['Jane', 'Mark', 'Bob', 'Emily'],
-//   job: 'teacher',
-//   isMarried: false,
-//   calcAge: function() {
-//     this.age = 2018 - this.birthYear;
-//   }
-// };
-//
-// john.calcAge();
-// console.log(john);
-// John = {};
+*/
+
+/******************************
+* Loops
+ */
+
+/*
+// For Loops
+for (var i = 0; i <= 20; i++) {
+    console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+  console.log(john[i]);
+}
+// i = 0, 0 < 10 true, log i to console, i++
+// i = 1, 1 , 10 true, log i to the console, i++
+// ...
+// i = 9, 1 < 10 true, log i to the console, i++
+// i = 10, 1 < 10 false, exit the loop!
+
+// While Loops
+var i = 0;
+while ( i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+
+// continue and break statements
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping Backwards
+for (i = john.length-1; i >= 0; i--) {
+  console.log(john[i]);
+}
