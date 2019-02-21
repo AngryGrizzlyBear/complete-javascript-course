83c1516d2a67998370d822cb813b26578afcee67
Evan <evanmlongwood@gmail.com>
2019-02-20 16:42:48 -0800

    Objects and methods.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -469,6 +469,7 @@ console.log(billWithTip);
 * Objects and properties
  */
 
+/*
 // Object literal
 var john = {
   firsName: 'John',
@@ -494,3 +495,23 @@ jane.name = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+/**********************************
+* Objects and methods
+ */
+
+var john = {
+  firsName: 'John',
+  lastName: 'Smith',
+  birthYear: 1992,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+   this.age = 2018 - this.birthYear;
+  }
+};
+
+john.calcAge();
+console.log(john);
