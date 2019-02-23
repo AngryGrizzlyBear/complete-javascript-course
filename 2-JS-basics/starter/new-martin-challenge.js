e9bfdced30b3bdc56624f56c24b70001b2a8c9a6
Evan <evanmlongwood@gmail.com>
2019-02-22 16:24:04 -0800

    array practice

--- 2-JS-basics/starter/new-martin-challenge.js
+++ 2-JS-basics/starter/new-martin-challenge.js
@@ -26,3 +26,8 @@ function translate_acids(str) {
 let results = translate_acids('ZGTCTTACX'); // should return ['guanine', 'thymine',
 // 'cytosine', 'thymine', 'thymine', 'adenine', 'cytosine']
 console.log(results);
+
+// const a = [ 1, 2, 3, 4, 5 ];
+// console.log(a[2],a[4]);
+const a = [ [ 0, 3 ], [ 'a', 'b', 'c' ], { hello: 'world', evan: 'grizzly' } ];
+console.log(a[0][1], a[1][1], a[2].hello, a[2].evan);
