488ea3f7c4d93383c86d7bc1258ceb3ba841cb1c
Evan <evanmlongwood@gmail.com>
2019-02-25 13:12:22 -0800

    Hoisting in practice.

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,17 +1,30 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
+calculateAge(1990);
 
+var retirement = function(year) {
+    console.log(65- (2016 - year))
+};
 
+retirement(1990);
 
+// variable
 
+var age = 23;
+console.log(age);
 
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
